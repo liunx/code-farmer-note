16feb30e166203fe028186239c70a58db3513bf3
Liunx <liunx163@163.com>
2013-03-17 21:20:51 +0800

    readjust the examples.
    we won't track the examples, instead, we just archive the examples
    to be  a full download package.

--- examples/site-to-site/ipsec-switch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash - 
-
-set -o nounset                              # Treat unset variables as an error
-
-echo "begin switch..."
-vde_switch --daemon --sock /tmp/ipsec.switch --mgmt /tmp/ipsec.mgmt
-slirpvde -d -s /tmp/ipsec.switch -dhcp
-echo "OK"
-
